86ec3518f63692cbd8f619241c7e4c416a661002
takenori15 <tak2kk@gmail.com>
2017-11-20 19:11:54 +0900

    add default user icon and restriction of user access

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, except: :show
   def show
     @user = User.find(params[:id])
   end
@@ -10,10 +11,12 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+    redirect_to root_path unless @user.id == current_user.id
   end
 
   def change_user_name
     @user = User.find(params[:id])
+    redirect_to root_path unless @user.id == current_user.id
   end
 
   def update_user_name
